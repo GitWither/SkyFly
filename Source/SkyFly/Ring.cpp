17ec76f1000fafb5d3af750579e82b7b24bef2cb
Wither <wither.tweets@gmail.com>
2020-06-07 19:08:45 +0200

    Wrote mesh getter for ring

--- Source/SkyFly/Ring.cpp
+++ Source/SkyFly/Ring.cpp
@@ -4,13 +4,25 @@
 #include "Ring.h"
 
 // Sets default values
-ARing::ARing()
+ARing::ARing() :
+	Score(15)
 {
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
 	Mesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Mesh"));
 	Mesh->SetupAttachment(RootComponent);
+
+	Niagara = CreateDefaultSubobject<UNiagaraSystem>(TEXT("Niagara"));
+	
+	static ConstructorHelpers::FObjectFinder<UStaticMesh> RingVisualAsset(TEXT("/Game/Geometry/Ring.Ring"));
+
+	if (RingVisualAsset.Succeeded()) {
+		Mesh->SetStaticMesh(RingVisualAsset.Object);
+		Mesh->SetRelativeLocation(FVector(0.0f, 0.0f, 0.0f));
+		Mesh->SetCollisionProfileName(TEXT("Trigger"));
+		Mesh->SetSimulatePhysics(false);
+	}
 }
 
 // Called when the game starts or when spawned
